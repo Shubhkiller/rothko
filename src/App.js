b15c9c16e635ee0faec91c6903ca3fe405aae520
Shubhkiller <shubhamtripathi033@gmail.com>
2024-01-13 12:32:33 +0530

    changes

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="frame">
+  <div className="canvas">
+    <div className="one" />
+    <div className="two" />
+    <div className="three" />
+  </div>
+</div>
+
+  );
+}
+
+export default App;
